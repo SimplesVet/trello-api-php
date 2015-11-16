3cf8735de67a5152606d781a26dd9052e75e9d1b
Vinicius Dias <vncdias@hotmail.com>
2015-11-16 15:09:49 -0300

    Refactor GET, POST, PUT and DELETE methods Now, methods are on Trello class, being inherited from the other classes, avoiding retyping code.

--- example/example.php
+++ example/example.php
@@ -4,8 +4,8 @@ header('Content-Type: text/html; charset=utf-8');
 
 require '../src/trello.php';
 
-$key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-$token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+$key = 'ceffa4a5badbcfb28f8bb92eb34db123';
+$token = '8601b7f8825fb51b17621a54a269dffa20da0f9586d8f71f85e5d8597773a605';
 
 try {
 	echo '<pre>';
@@ -14,26 +14,26 @@ try {
 	// BOARD
 	// -----
 	// API Reference: https://developers.trello.com/advanced-reference/board
-	echo '<h1>Board</h1>';
+	// echo '<h1>Board</h1>';
 
-	$trelloBoard = new TrelloBoard($key, $token);
-	$boardId = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+	// $trelloBoard = new TrelloBoard($key, $token);
+	// $boardId = 'xxxxxxxxxxxxxxxxxxxxxxxx';
 
-	// Getting board info
-	$return = $trelloBoard->get($boardId);
-	print_r($return);
-	echo '<hr>';
+	// // Getting board info
+	// $return = $trelloBoard->get($boardId);
+	// print_r($return);
+	// echo '<hr>';
 
-	// Getting list members from board
-	$return = $trelloBoard->get($boardId, 'members');
-	print_r($return);
-	echo '<hr>';
+	// // Getting list members from board
+	// $return = $trelloBoard->get($boardId, 'members');
+	// print_r($return);
+	// echo '<hr>';
 
-	// Getting list cards from board
-	$arguments = array('fields' => 'id,name,idList');
-	$return = $trelloBoard->get($boardId, 'cards', $arguments);
-	print_r($return);
-	echo '<hr>';
+	// // Getting list cards from board
+	// $arguments = array('fields' => 'id,name,idList');
+	// $return = $trelloBoard->get($boardId, 'cards', $arguments);
+	// print_r($return);
+	// echo '<hr>';
 
 	// ----
 	// CARD
@@ -42,27 +42,31 @@ try {
 	echo '<h1>Card</h1>';
 
 	$trelloCard = new TrelloCard($key, $token);
-	$cardId = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+	$cardId = '564498b15373ee55441f6d98';
 
 	// Getting Card Info
-	$return = $trelloCard->get($cardId);
-	print_r($return);
-	echo '<hr>';
+	// $return = $trelloCard->get($cardId);
+	// print_r($return);
+	// echo '<hr>';
 
-	// Getting Card Actions
-	$return = $trelloCard->get($cardId, 'actions');
-	print_r($return);
-	echo '<hr>';
+	// // Getting Card Actions
+	// $return = $trelloCard->get($cardId, 'actions');
+	// print_r($return);
+	// echo '<hr>';
 
-	// Creating a Card
-	$data = array(
-		'name' => 'Teste card ' . rand(),
-		'desc' => 'This is a test card',
-		'due' => null,
-		'idList' => 'xxxxxxxxxxxxxxxxxxxxxxxx',
-		'urlSource' => null
-	);
-	$return = $trelloCard->post($data);
+	// // Creating a Card
+	// $data = array(
+	// 	'name' => 'Teste card ' . rand(),
+	// 	'desc' => 'This is a test card',
+	// 	'due' => null,
+	// 	'idList' => '5644869ae27ff117e1cef3da',
+	// 	'urlSource' => null
+	// );
+	// $return = $trelloCard->post($data);
+	// print_r($return);
+	// echo '<hr>';
+
+	$return = $trelloCard->comment('5649e6065320a3ee5c16e218', "testando 123");
 	print_r($return);
 	echo '<hr>';
 
--- src/trello-board.php
+++ src/trello-board.php
@@ -6,20 +6,5 @@
 */
 class TrelloBoard extends Trello
 {
-	public function get($boardId, $complement = '', $arguments = array()) {
-		try {
-			$authParam = $this->getAuthParam();
-			$url = $this->getUrl();
-
-			$argumentsParam = '';
-			if(!empty($arguments)){
-				$argumentsParam = '&' . http_build_query($arguments);
-			}
-
-			$this->curl->get($url . 'boards/' . $boardId . '/' . $complement . $authParam . $argumentsParam);
-			return $this->curl->response;
-		} catch (Exception $e) {
-			throw new Exception($e->getMessage());
-		}
-	}
+	public $collection = "boards";
 }
